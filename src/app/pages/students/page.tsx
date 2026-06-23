3e6da17d7ce1b4ea1615f6e169acc72f8d355c8d
raoneinet <raoneinet@hotmail.com>
2026-06-23 22:58:55 +0100

    fea: table created

--- src/app/pages/students/page.tsx
+++ src/app/pages/students/page.tsx
@@ -1,40 +1,59 @@
+import { use } from "react"
 import { Header } from "@/components/header/header"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Pen, Trash } from "lucide-react"
 
 const StudentPage = () => {
+
+    const students = use(fetch("http://localhost:3001/students").then(item => item.json()))
+    console.log(students)
+
+
     return (
         <div className="w-full p-10 ">
             <Header />
             <div className=" py-10">
-                <table className="w-full p-10 text-center">
-                    <thead className="min-w-full">
-                        <th className="w-full flex justify-between">
-                            <td>Students</td>
-                            <td>Gender</td>
-                            <td>Age</td>
-                            <td>Class</td>
-                            <td>Avg. Grade</td>
-                            <td>email</td>
-                            <td>Actions</td>
-                        </th>
-                    </thead>
-                    <tbody className="w-full text-center">
-                        <tr className="w-full flex justify-between items-center py-3 px-2 border-b border-gray-300 hover:bg-gray-300">
-                            <td>
-                                <div>Img</div>
-                                <div>Robert Fox</div>
-                            </td>
-                            <td>Male</td>
-                            <td>18</td>
-                            <td>English</td>
-                            <td>9.2</td>
-                            <td>robertfox@gmail.com</td>
-                            <td>
-                                <div>Editar</div>
-                                <div>Excluir</div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Table className="border">
+                    <TableCaption>List of students.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-25">Name</TableHead>
+                            <TableHead className="w-25">Gender</TableHead>
+                            <TableHead className="w-25">Age</TableHead>
+                            <TableHead className="w-25">Class</TableHead>
+                            <TableHead className="w-25">Avg. Grade</TableHead>
+                            <TableHead className="w-25">email</TableHead>
+                            <TableHead className="w-25">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {students.data.map((item: any) => (
+                            <TableRow>
+                                <TableCell className="font-medium flex items-center py-4">
+                                    <div>Img</div>
+                                    <div>{item.name}</div>
+                                </TableCell>
+                                <TableCell className="font-medium">{item.gender}</TableCell>
+                                <TableCell className="font-medium">{item.age}</TableCell>
+                                <TableCell className="font-medium">{item.class}</TableCell>
+                                <TableCell className="font-medium">{item.avgGrade}</TableCell>
+                                <TableCell className="font-medium">{item.email}</TableCell>
+                                <TableCell className="font-medium flex gap-4 h-full">
+                                    <Pen size="16" />
+                                    <Trash size="16" />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
         </div>
     )
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,15 @@
-export const Header = ()=>{
+import { Plus } from "lucide-react"
+import { Button } from "../ui/button"
+
+export const Header = () => {
     return (
-        <div className="flex justify-between w-full bg-amber-200">
-            <div>Title</div>
-            <div>Actions</div>
+        <div className="flex justify-between w-full">
+            <div>STUDENTS</div>
+            <div>
+                <Button className="cursor-pointer" type="button">
+                    <Plus size="35" />
+                </Button>
+            </div>
         </div>
     )
 }
